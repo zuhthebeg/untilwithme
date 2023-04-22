3bb101964f5b69bd3b560d6fcb88444d15300a16
cocy <zuhthebeg@naver.com>
2023-04-23 02:13:31 +0900

    init

---
+++ script.js
@@ -0,0 +1,33 @@
+/*-----
+Spanizer
+- Wraps letters with spans, for css animations
+-----*/
+(function($) {
+    var s,
+    spanizeLetters = {
+      settings: {
+        letters: $('.js-spanize'),
+      },
+      init: function() {
+        s = this.settings;
+        this.bindEvents();
+      },
+      bindEvents: function(){
+        s.letters.html(function (i, el) {
+          //spanizeLetters.joinChars();
+          var spanizer = $.trim(el).split("");
+          return '<span>' + spanizer.join('</span><span>') + '</span>';
+        });
+      },
+    };
+    spanizeLetters.init();
+  })(jQuery);
+
+const flowerImg = document.getElementById('flower-img');
+const count = document.getElementById('count');
+let numClicks = 0;
+
+flowerImg.addEventListener('click', () => {
+  numClicks++;
+  count.innerText = numClicks;
+});
